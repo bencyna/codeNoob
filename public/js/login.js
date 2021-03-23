9f7516f83e5df867fbdb015cd7bd97a550d5fbdf
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-03-23 20:34:18 +1030

    modals impl

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,52 @@
-const loginFormHandler = async (event) => {
-  event.preventDefault();
+// Get the modal
+var modal = document.getElementById("loginModal");
 
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+// Get the button that opens the modal
+var logBtn = document.getElementById("loginButt");
 
-  console.log(email + password);
-  if (email && password) {
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      console.log(response);
+// Get the <span> element that closes the modal
+var closeLoginModalBtn = document.getElementById("closeLoginModal");
 
-      document.location.replace("/");
-    } else {
-      console.log("Failed to log in");
+// When the user clicks the button, open the modal 
+logBtn.onclick = function() {
+    modal.classList.add("is-active");
+}
+
+// When the user clicks on <span> (x), close the modal
+closeLoginModalBtn.onclick = function() {
+    modal.classList.remove("is-active");
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-  }
+}
+
+const loginFormHandler = async(event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
+
+    console.log(email + password);
+    if (email && password) {
+        const response = await fetch("/api/users/login", {
+            method: "POST",
+            body: JSON.stringify({ email, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.ok) {
+            console.log(response);
+
+            document.location.replace("/");
+        } else {
+            console.log("Failed to log in");
+        }
+    }
 };
 
 document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+    .querySelector(".login-form")
+    .addEventListener("submit", loginFormHandler);
