472565bce59e3defea62771e6ed458f5e29a950b
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-03-25 20:22:30 +1030

    signup is done, commets done, update routes, and added first name, last name in the form

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
-const { User, Post } = require("../../models");
-const withAuth = require('../../utils/auth');
-router.post('/dashboard', async (req, res)=>{
+const { User, Post, Comment } = require("../../models");
+const withAuth = require('../../utils/auth'); // need to make plan for this how we can use the auth in the post
+
+router.post('/dashboard',withAuth, async (req, res)=>{
     try {
-        const postData = await Post.create({
-            post: req.body.content,
-            title: req.body.title,
-            user_id: req.session.user_id
-        }); 
+        const postData = await Post.create(
+           req.body
+        ); 
         res.status(200).json(postData);
     } catch (error) {
         res.status(400).json(error);
@@ -33,20 +32,60 @@ router.get('/dashboard', async (req, res)=>{
     }
 });
 
-router.put('/dashboard/:id',async (req,res)=>{
+router.get('/dashboard/:id', async (req, res)=>{
+    const posts = await Post.findOne({
+        where: {
+            // user_id: "4c877132-1e9c-4e41-ad04-1568e1ee6ce6",
+            id: req.params.id 
+          },
+          include:[
+            {
+              model: User,
+              attributes: ["first_name", "last_name"],
+            },
+            {
+                model:Comment,
+                attributes:['comments']
+            }
+          ]
+    });
+    try {
+        res.json( posts);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+// need to think
+router.put('/dashboard/:id',withAuth, async (req,res)=>{
 try {
     const updatePostData = await Post.update({
         title: req.body.title,
-        post: req.body.content
+        post: req.body.post
     },{
      where: {
-         id: req.body.id,
+         id: req.params.id,
        }
     });
     res.status(200).json(updatePostData);            
 } catch (error) {
-    res.status(500).json(error);    
+    res.status(400).json(error);    
 }});
 
+router.delete('/dashboard/:id',withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
