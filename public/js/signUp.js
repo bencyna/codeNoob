0529e4929c5445fc190eb5a91ab9ea1d57d8e9ba
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2021-03-28 14:29:22 +1030

    login and signup buttons work outside of main page now

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,59 +1,61 @@
-// Get the modal
-var signModal = document.getElementById("signModal");
+const signModal = document.getElementById("signModal");
 
 // Get the button that opens the modal
-var signBtn = document.getElementById("signButt");
+const signBtn = document.getElementById("signButt");
 
-var closeSignupModalBtn = document.getElementById("closeSignupModal");
+const closeSignupModalBtn = document.getElementById("closeSignupModal");
 
-// When the user clicks the button, open the modal 
-if(signBtn){
-    signBtn.addEventListener("click", function() {
-        signModal.classList.add("is-active");
-    }, false);
-}
+const commentSignup = document.getElementById("signUpComment");
 
+// When the user clicks the button, open the modal
+document.addEventListener("click", function (event) {
+  if (event.target !== signBtn && event.target !== commentSignup) {
+    return;
+  }
+  signModal.classList.add("is-active");
+});
 
 // When the user clicks on <span> (x), close the modal
-closeSignupModalBtn.addEventListener("click", function() {
-    signModal.classList.remove("is-active");
-})
+closeSignupModalBtn.addEventListener("click", function () {
+  signModal.classList.remove("is-active");
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == signModal) {
-        signModal.classList.remove("is-active");
-        signModal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == signModal) {
+    signModal.classList.remove("is-active");
+    signModal.style.display = "none";
+  }
+};
 
 async function signupFormHandler(event) {
-    event.preventDefault();
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
-    const firstName = document.querySelector("#first-name-signup").value.trim();
-    const lastName = document.querySelector("#last-name-signup").value.trim();
+  event.preventDefault();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  const firstName = document.querySelector("#first-name-signup").value.trim();
+  const lastName = document.querySelector("#last-name-signup").value.trim();
 
-
-    if (email && password && firstName && lastName) {
-        const response = await fetch("/api/users/signup", {
-            method: "POST",
-            body: JSON.stringify({
-                firstName,
-                lastName,
-                email,
-                password,
-            }),
-            headers: { "Content-Type": "application/json" },
-        });
-        if (response.ok) {
-            console.log("success");
-            document.location.replace("/");
-        } else {
-            alert(response.statusText);
-        }
+  if (email && password && firstName && lastName) {
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      alert(
+        "you have successfully signed up! Login to use all the cool features!"
+      );
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 document
-    .querySelector(".signup-form")
-    .addEventListener("submit", signupFormHandler);
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
